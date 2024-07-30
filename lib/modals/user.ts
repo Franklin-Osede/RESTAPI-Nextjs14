d31661d443686e4d8a9f5868b7a83c40f085a9c7
Franklin <franklin.op@hotmail.com>
2024-07-30 10:25:20 +0200

    created deleted user function

--- lib/modals/user.ts
+++ lib/modals/user.ts
@@ -7,6 +7,10 @@ const UserSchema = new Schema (
         password: {type: "string", required: true}
     },
     {
-
+       timestamps:true
     }
 )
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
