04a76c2e2975e64252aa514726ce48af403375c6
Franklin <franklin.op@hotmail.com>
2024-08-04 09:40:55 +0200

    updated route blog

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -4,7 +4,6 @@ import Blog from "@/lib/modals/category";
 import Category from "@/lib/modals/category";
 import {Types} from "mongoose";
 import User from "@/lib/modals/category";
-import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 
 export const GET = async (request: Request, context: {params: any})=>{
@@ -130,5 +129,53 @@ export const PATCH = async (request: Request, context:{params:any})=>{
         return new NextResponse("Error updating blog" + error.message, {
             status:500
         });
+    }
+}
+
+export const DELETE = async (request: Request, context: {params: any})=>{
+    const blogId = context.params.blog;
+    try {
+        const {searchParams} = new URL(request.url);
+        const userId = searchParams.get("userId");
+
+        if(!userId || !Types.ObjectId.isValid(userId)){
+            return new NextResponse(
+                JSON.stringify({message: "Invalid or missing userId"}),
+                {status: 400}
+            );
+        }
+
+        if(!blogId || !Types.ObjectId.isValid(blogId)){
+         return new NextResponse(
+            JSON.stringify({message: "Invalid or missing blogId"}),
+            {status: 400 }
+         )
+        }
+
+        await connect()
+
+        const user = await User.findById(userId);
+        if(!user){
+            return new NextResponse(JSON.stringify({message: "User not found"}), {
+                status: 400
+            });
+        }
+
+        const blog = await Blog.findOne({ _id: blogId, user: userId});
+        if(!blog){
+            return new NextResponse(JSON.stringify({message: "Blog not found"}),{
+                status:404
+            })
+        }
+
+        await Blog.findByIdAndDelete(blogId);
+
+        return new NextResponse(JSON.stringify({message: "Blog is deleted"}), {
+            status: 200,
+        })
+    } catch (error:any) {
+        return new NextResponse("Error in deleting blog" + error.message, {
+            status:500,
+        })
     }
 }
